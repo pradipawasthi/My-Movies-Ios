00d90c3ddecbb32c44f782e020f7a71ca3f08e7d
Prakhar Parakh <prakharparakh.10@gmail.com>
2024-08-03 15:26:56 +0530

    [WIP]: Home page background

--- My-Movies-Ios/View/HomeView.swift
+++ My-Movies-Ios/View/HomeView.swift
@@ -8,8 +8,36 @@
 import SwiftUI
 
 struct HomeView: View {
+  @State var searchText = ""
+
   var body: some View {
-    Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    ZStack {
+      Color.background
+        .ignoresSafeArea()
+      Circle()
+        .frame(width: 250, height: 200)
+        .offset(x: -UIScreen.main.bounds.width / 2, y: -UIScreen.main.bounds.height / 2)
+        .foregroundStyle(Color.green100.opacity(0.5))
+        .blur(radius: 50)
+      
+      Circle()
+        .frame(width: 350, height: 300)
+        .offset(x: UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 4 - 200)
+        .foregroundStyle(Color.pink100.opacity(0.4))
+        .blur(radius: 50)
+      
+      Circle()
+        .frame(width: 250, height: 300)
+        .offset(x: -UIScreen.main.bounds.width / 2, y: UIScreen.main.bounds.height / 2)
+        .foregroundStyle(Color.green100.opacity(0.4))
+        .blur(radius: 50)
+      
+      ScrollView(showsIndicators: false) {
+        LazyVStack {
+          
+        }
+      }
+    }
   }
 }
 
---
+++ My-Movies-Ios/View/SearchView.swift
@@ -0,0 +1,24 @@
+//
+//  SearchView.swift
+//  My-Movies-Ios
+//
+//  Created by Prakhar Parakh on 03/08/24.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+  @State var searchText = ""
+  var body: some View {
+    HStack {
+      
+    }
+    .background(
+      RoundedRectangle(cornerRadius: 10)
+    )
+  }
+}
+
+#Preview {
+  SearchView()
+}
